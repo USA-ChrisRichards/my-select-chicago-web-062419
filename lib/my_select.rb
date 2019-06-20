2051922292c808c417af75155b181814db8897e6
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-20 16:38:27 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
-def my_select(collection)
- # your code here!
+def my_collect(collection)
+  final_collection = []
+  n = 0
+  while n < collection.size
+    element = collection[n]
+    n = n + 1
+    final_collection << yield(element)
+  end
+   final_collection
 end

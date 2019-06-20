660b495f991913db29da393a60dfc5f9a65ae00d
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-20 16:46:54 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
   while n < collection.size
     element = collection[n]
     n = n + 1
-    if block == true
+    if block_given? == true
       final_collection << element
     end
   end
